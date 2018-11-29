8b20b0ab551a2d532e5c4444b79411d378c02918
Md. Zeeshan HAIDER <mdzeeshan.haider.external@axa.be>
2018-11-29 18:57:15 +0530

    Validation in Registration page partially done

--- src/app/modules/general/component/registration-page/registration-page.component.ts
+++ src/app/modules/general/component/registration-page/registration-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration-page',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationPageComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    this.BuildForm();
+  }
+  public regisForm: FormGroup;
 
   ngOnInit() {
+  }
+
+  userRegis(){
+
+  }
+
+  BuildForm() {
+
+    const DecimalPattern: any = '^(?:[0-9]+(?:\.[0-9]{0,2})?)?$';
+
+    this.regisForm = new FormGroup({
+      firstName: new FormControl({value: '', disabled: true}),
+    });
+
   }
 
 }
